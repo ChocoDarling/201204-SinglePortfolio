8ab2dca4e0c1b05dc8aa144c60ece853e4d3fff5
ChocoDarling <35556229+ChocoDarling@users.noreply.github.com>
2020-12-17 11:54:53 +0900

    201217-title-change-and-auth-error-check

--- server/api/auth/auth.ctrl.js
+++ server/api/auth/auth.ctrl.js
@@ -4,7 +4,7 @@ const User = require('../../models/user');
 const register = async (ctx) => {
   const schema = Joi.object().keys({
     username: Joi.string().alphanum().min(5).max(20).required(),
-    name: Joi.string().alphanum().min(5).max(20).required(),
+    name: Joi.string().min(2).max(20).required(),
     phone: Joi.string().min(10).max(11).required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
